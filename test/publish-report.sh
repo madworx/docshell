a0594ad2c948b9d4b247238781046ea44c5278d3
Martin Kjellstrand <github@madworx.se>
2018-07-18 12:02:33 +0200

    Test out using the cd-ci-glue library for Wiki publishing. (#10)

--- test/publish-report.sh
+++ test/publish-report.sh
@@ -4,26 +4,18 @@ set -eE
 set -o pipefail
 set -x
 
-if [ "${TRAVIS_EVENT_TYPE}" == "push" ] ; then
-    if [ "${TRAVIS_BRANCH}" == "master" ] ; then
-        echo "Push on \`master' branch:"
-        echo "  - generating compatability matrix and pushing to Github Wiki."
+# shellcheck disable=SC1090
+source <(curl https://raw.githubusercontent.com/madworx/cd-ci-glue/master/src/cd-ci-glue.bash)
 
-        TMPDR="$(mktemp -d)"
+is_travis_master_push && (
+    echo "Push event on \`master' branch. Publishing to Wiki"
 
-        git config --global user.email "travis@travis-ci.org"
-        git config --global user.name  "Travis CI"
-        git clone "https://${GH_TOKEN}@github.com/madworx/docshell.wiki.git" "${TMPDR}"
-
-        cp ../README.md "${TMPDR}/Home.md"
-        make -s generate-report DIR="${DIR}" > "${TMPDR}/Compatibility-matrix.md"
-
-        cd "${TMPDR}"
-        git commit -m 'Automated generation of compatability matrix' Compatibility-matrix.md Home.md || exit 0
-        git push
-        exit 0
-    fi
-fi
+    TMPDR=$(github_wiki_prepare madworx/docshell)
+    cp ../README.md "${TMPDR}/Home.md"
+    make -s generate-report DIR="${DIR}" > "${TMPDR}/Compatibility-matrix.md"
+    github_wiki_commit "${TMPDR}"
+    exit 0
+)
 
 echo "Not on \`master' branch - not publishing, only printing."
 make -s generate-report DIR="${DIR}"
