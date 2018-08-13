d8a6ba6156863bcaddd63507b3f3db3564db9f1e
Martin Kjellstrand <github@madworx.se>
2018-08-13 15:36:56 +0200

    Retain old test results / compatibility matrix (#14)
    * Retain old test results by comitting them to the wiki repo.
    * Sorting won't work on the tap files.
    This "workaround" will create hueg amounts of data, but will suffice for now.
    * Fixes for git reset.
    * Add NetBSD 8.0 to test suite.

--- test/test-all.sh
+++ test/test-all.sh
@@ -9,7 +9,7 @@ for VER in madworx/debian-archive:{etch,lenny,squeeze} \
            ubuntu:{artful,bionic,trusty,xenial,zesty,yakkety,precise} \
            centos:{6,7} \
            opensuse/{leap,tumbleweed} \
-           madworx/netbsd:{7.1.2,6.1.5}-x86_64 ; do
+           madworx/netbsd:{7.1.2,6.1.5,8.0}-x86_64 ; do
     if [[ "${VER}" = *${REQUESTED}* ]] ; then
         echo "Testing image ${VER}" 1>&2
 
